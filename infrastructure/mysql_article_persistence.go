b2e51e16564d3e6937be7256e31b92bd3c49c3d0
take-2405 <take12.wa@icloud.com>
2022-01-31 10:36:04 +0900

    wip fix deleteが全部のデータを消してしまう

--- infrastructure/mysql_article_persistence.go
+++ infrastructure/mysql_article_persistence.go
@@ -30,8 +30,10 @@ func (a *articlePersistence) CreateNewArticle(articleID, title, description, con
 	article.Contents = content
 	article.Nice = 0
 
-	str := "INSERT INTO article_tags (`id`,`article_id`,`tag`) VALUES"
-	var s []string
+	bulkInsertTagQuery := "INSERT INTO article_tags (`id`,`article_id`,`tag`) VALUES"
+	bulkInsertImageQuery := "INSERT INTO article_images (`id`,`article_id`,`image`) VALUES"
+	var bulkInsertTag []string
+	var bulkInsertImage []string
 	var queryEnd string
 
 	for i, tag := range tags {
@@ -46,10 +48,25 @@ func (a *articlePersistence) CreateNewArticle(articleID, title, description, con
 			return err
 		}
 		q := fmt.Sprintf("('%s', '%s','%s')%s", tagID.String(), articleID, tag, queryEnd)
-		s = append(s, q)
+		bulkInsertTag = append(bulkInsertTag, q)
 	}
+	bulkInsertTagQuery += strings.Join(bulkInsertTag, "")
 
-	query := strings.Join(s, "")
+	for i, image := range images {
+		if len(images)-1 == i {
+			queryEnd = ";"
+		} else {
+			queryEnd = ","
+		}
+		imageID, err := uuid.NewRandom()
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		q := fmt.Sprintf("('%s', '%s','%s')%s", imageID.String(), articleID, image, queryEnd)
+		bulkInsertImage = append(bulkInsertImage, q)
+	}
+	bulkInsertImageQuery += strings.Join(bulkInsertImage, "")
 
 	err := a.mysql.Client.Transaction(func(tx *gorm.DB) error {
 		//記事を追加
@@ -58,7 +75,12 @@ func (a *articlePersistence) CreateNewArticle(articleID, title, description, con
 			return err
 		}
 		//タグを追加
-		if err := tx.Exec(str + query).Error; err != nil {
+		if err := tx.Exec(bulkInsertTagQuery).Error; err != nil {
+			log.Println(err)
+			return err
+		}
+		//画像を追加
+		if err := tx.Exec(bulkInsertImageQuery).Error; err != nil {
 			log.Println(err)
 			return err
 		}
@@ -76,12 +98,8 @@ func (a *articlePersistence) CreateNewArticle(articleID, title, description, con
 
 func (a *articlePersistence) FixArticle(articleID, title, description, content, userID string, tags, images []string) error {
 	var article table.Articles
+
 	if err := a.mysql.Client.Raw("SELECT * FROM articles WHERE id = ? AND user_id = ?", articleID, userID).Scan(&article).Error; err != nil {
-		log.Println(err)
-		return err
-	}
-	if article.Title == "" {
-		err := errors.New("this request user has not this article.")
 		log.Println(err)
 		return err
 	}
@@ -97,6 +115,85 @@ func (a *articlePersistence) FixArticle(articleID, title, description, content, 
 	if description != "" {
 		article.Description = description
 	}
+
+	if article.Title == "" {
+		err := errors.New("this request user has not this article.")
+		log.Println(err)
+		return err
+	}
+	if len(tags) > 0 {
+		bulkInsertTagQuery := "INSERT INTO article_tags (`id`,`article_id`,`tag`) VALUES"
+		var bulkInsertTag []string
+		var queryEnd string
+
+		for i, tag := range tags {
+			if len(tags)-1 == i {
+				queryEnd = ";"
+			} else {
+				queryEnd = ","
+			}
+			tagID, err := uuid.NewRandom()
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+			q := fmt.Sprintf("('%s', '%s','%s')%s", tagID.String(), articleID, tag, queryEnd)
+			bulkInsertTag = append(bulkInsertTag, q)
+		}
+		bulkInsertTagQuery += strings.Join(bulkInsertTag, "")
+
+		a.mysql.Client.Transaction(func(tx *gorm.DB) error {
+			var deleteTag table.ArticleTag
+			deleteTag.ArticleID = articleID
+			if err := tx.Delete(&deleteTag).Error; err != nil {
+				log.Println(err)
+				return err
+			}
+
+			if err := tx.Exec(bulkInsertTagQuery).Error; err != nil {
+				log.Println(err)
+				return err
+			}
+			return nil
+		})
+	}
+
+	if len(images) > 0 {
+		bulkInsertImageQuery := "INSERT INTO article_images (`id`,`article_id`,`image`) VALUES"
+		var bulkInsertImage []string
+		var queryEnd string
+
+		for i, image := range images {
+			if len(images)-1 == i {
+				queryEnd = ";"
+			} else {
+				queryEnd = ","
+			}
+			imageID, err := uuid.NewRandom()
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+			q := fmt.Sprintf("('%s', '%s','%s')%s", imageID.String(), articleID, image, queryEnd)
+			bulkInsertImage = append(bulkInsertImage, q)
+		}
+		bulkInsertImageQuery += strings.Join(bulkInsertImage, "")
+
+		a.mysql.Client.Transaction(func(tx *gorm.DB) error {
+			var deleteImage table.ArticleImage
+			deleteImage.ArticleID = articleID
+			if err := tx.Delete(&deleteImage).Error; err != nil {
+				log.Println(err)
+				return err
+			}
+			if err := tx.Exec(bulkInsertImageQuery).Error; err != nil {
+				log.Println(err)
+				return err
+			}
+			return nil
+		})
+	}
+
 	if err := a.mysql.Client.Save(&article).Error; err != nil {
 		log.Println(err)
 		return err
@@ -107,6 +204,11 @@ func (a *articlePersistence) FixArticle(articleID, title, description, content, 
 
 func (a *articlePersistence) DeleteArticle(articleID, userID string) error {
 	var article table.Articles
+	var image table.ArticleImage
+	var tag table.ArticleTag
+	image.ArticleID = articleID
+	tag.ArticleID = articleID
+
 	if err := a.mysql.Client.Raw("SELECT * FROM articles WHERE id = ? AND user_id = ?", articleID, userID).Scan(&article).Error; err != nil {
 		log.Println(err)
 		return err
@@ -117,10 +219,22 @@ func (a *articlePersistence) DeleteArticle(articleID, userID string) error {
 		return err
 	}
 
-	if err := a.mysql.Client.Delete(&article).Error; err != nil {
-		log.Println(err)
-		return err
-	}
+	a.mysql.Client.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Delete(&article).Error; err != nil {
+			log.Println(err)
+			return err
+		}
+		if err := tx.Delete(&image).Error; err != nil {
+			log.Println(err)
+			return err
+		}
+		if err := tx.Delete(&tag).Error; err != nil {
+			log.Println(err)
+			return err
+		}
+		return nil
+	})
+
 	return nil
 }
 
