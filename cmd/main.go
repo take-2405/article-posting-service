0565a1468a28df74f7ad98884953bf396190a7b6
take-2405 <take12.wa@icloud.com>
2022-02-03 18:06:27 +0900

    add use pprof and cpu performance check

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"net/http"
+	_ "net/http/pprof"
 	"prac-orm-transaction/config"
 	"prac-orm-transaction/di"
 	router2 "prac-orm-transaction/presentation/router"
 )
 
 func main() {
+
 	//ルーターを初期化
 	router := router2.NewServer()
 	//ルーティングとDI
 	di.InsertUserDI(router)
-
 	port := config.GetServerPort()
 	//ルーター起動
 	http.ListenAndServe(port, router.Router)
